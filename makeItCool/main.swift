fb7eb814e6c4862a57b8ceadaced91c745d378e5
ilyayurchenko <ilya09yurchenko@gmail.com>
2022-11-15 21:50:12 +0200

    Added function to make string cooler

--- makeItCool/main.swift
+++ makeItCool/main.swift
@@ -1,11 +1,8 @@
-//
-//  main.swift
-//  makeItCool
-//
-//  Created by Ilya Yurchenko on 15.11.2022.
-//
-
 import Foundation
 
-print("Hello, World!")
+func makeItCool(_ string: String) -> String {
+    let newCoolString = string.lowercased().replacingOccurrences(of: "a", with: "@").replacingOccurrences(of: "i", with: "1").replacingOccurrences(of: "s", with: "$").replacingOccurrences(of: "o", with: "0").replacingOccurrences(of: "t", with: "+")
+    return newCoolString
+}
 
+print(makeItCool("Swift is awesame"))
