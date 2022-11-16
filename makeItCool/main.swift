83713799c70231270e7eee2be85ba6958b4351bd
ilyayurchenko <ilya09yurchenko@gmail.com>
2022-11-16 15:59:32 +0200

    ups

--- makeItCool/main.swift
+++ makeItCool/main.swift
@@ -1,8 +1,49 @@
 import Foundation
+//
+//func makeItCool(_ string: String) -> String {
+//    let newCoolString = string.lowercased()
+//    .replacingOccurrences(of: "a", with: "@")
+//    .replacingOccurrences(of: "i", with: "1")
+//    .replacingOccurrences(of: "s", with: "$")
+//    .replacingOccurrences(of: "o", with: "0")
+//    .replacingOccurrences(of: "t", with: "+")
+//    return newCoolString
+//}
+//
+//print(makeItCool("Swift is awesame"))
+
+
+//func makeItCool(_ string: String) -> String {
+//    let newCoolString: String
+//    let replaces = [
+//         "@" : ["A", "a"],
+//         "0" : ["O", "o"],
+//         "+" : ["T", "t"],
+//         "1" : ["I", "i"],
+//         "$" : ["S", "s"],
+//        ]
+//
+//    for value in replaces.values {
+//        newCoolString = string.replacingOccurrences(of: value, with: key)
+//    }
+//
+//
+//    return newCoolString
+//}
 
 func makeItCool(_ string: String) -> String {
-    let newCoolString = string.lowercased().replacingOccurrences(of: "a", with: "@").replacingOccurrences(of: "i", with: "1").replacingOccurrences(of: "s", with: "$").replacingOccurrences(of: "o", with: "0").replacingOccurrences(of: "t", with: "+")
+    var newCoolString = ""
+    let replaces = [
+        "a" : "@",
+        "o" : "0",
+        "t" : "+",
+        "i" : "1",
+        "s" : "$",
+    ]
+    
+    for (key, value) in replaces {
+        newCoolString = string.lowercased().replacingOccurrences(of: key, with: value, options: .literal, range: nil)
+    }
     return newCoolString
 }
-
-print(makeItCool("Swift is awesame"))
+print(makeItCool("Swift is Awesame"))
